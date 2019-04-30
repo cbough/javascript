d94c2d015650c95e804666e040796b0e00a7a4ef
Christopher Bough <christopherbough85@gmail.com>
2019-04-30 13:49:29 -0400

    added java

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 let x = document.getElementById('profileBtn')
 let y = document.getElementsByClassName('para')
 
-console.log(y)
-
-
-
-let main = prompt('Who is your main?')// ask question, store answer in main variable
-console.log(main)// log main variable to the console
-
-// main variable is input from user. Which means it oculd look like anything
-
-if (main == "Roy" || main == 'roy') { // convert string in main to ensure it will be lower case
-  alert('That is correct!')
-} else {
-  alert('you suck!')
+// function functionName() {}
+function hiddenElement () {
+paras[3].classList.add("hidden")
 }
+
+// thisIsCalled CamelCase
+// This_is_called snake_case
+
+
+// takes two arguments: 1. the event, 2. the function to run when event happens
+paras[0].addEventListener('click', hideElement)
